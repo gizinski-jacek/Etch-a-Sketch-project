c2a9be6a5d6f2d423975e3ec1e7a50483d81273c
mrjackmaster <jacektrg+github@gmail.com>
2021-03-29 20:30:41 +0200

    Added function for resetting and setting up new grid. New function to reattach listeners on new grid.

--- EtchSketch.js
+++ EtchSketch.js
@@ -1,8 +1,10 @@
 const mainGridContainer = document.querySelector('#mainGridContainer');
+const resetBtn = document.querySelector('#resetBtn');
 
+// Create grid with specified sizes.
 function makeGrid(columns, rows) {
-    mainGridContainer.style.setProperty('--grid-columns', columns);
-    mainGridContainer.style.setProperty('--grid-rows', rows);
+    mainGridContainer.style.setProperty('--gridColumns', columns);
+    mainGridContainer.style.setProperty('--gridRows', rows);
     for (let c = 1; c <= (columns * rows); c++) {
         let gridElement = document.createElement('div');
         gridElement.className = 'gridElement';
@@ -10,4 +12,36 @@ function makeGrid(columns, rows) {
     }
 }
 
+// Create default 16x16 grid and attach event listeners.
 makeGrid(16, 16);
+reattachListener();
+
+// Watch for when mouse enters grid cell to change the cell color.
+function reattachListener() {
+    let gridCell = document.querySelectorAll('.gridElement');
+    gridCell.forEach((gridElement) => {
+        gridElement.addEventListener('mouseenter', () => {
+            gridElement.classList.add('hoverColor');
+        })
+    })
+}
+
+// Delete all grid cells.
+function resetGrid(container) {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+}
+
+// Button to delete grid, ask user how big new grid should be,
+// make new grid, attach new event listeners.
+resetBtn.addEventListener('click', function() {
+    resetGrid(mainGridContainer);
+    let newColumns = prompt('How many squares per side you want the grid to have?');
+        while (newColumns > 99) {
+            newColumns = prompt('Thats too many! Try less than 100.');
+        }
+    let newRows = newColumns;
+    makeGrid(newColumns, newRows);
+    reattachListener();
+})
